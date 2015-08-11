b5ceb6404d2caff09c85ce49652c6de2a73560b1
Scott Loarie <loarie@gmail.com>
2015-08-11 15:18:17 -0700

    secrets take 6

---
+++ config/initializers/twilio.rb
@@ -0,0 +1,8 @@
+# File: config/initializers/twilio.rb
+
+require 'twilio-ruby'
+
+Twilio.configure do |config|
+  config.account_sid = Rails.application.secrets.twilio_account_sid
+  config.auth_token  = Rails.application.secrets.twilio_auth_token
+end
