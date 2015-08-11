7d199b881ca07c0220696e5ff54ca0ba91384852
Scott Loarie <loarie@gmail.com>
2015-08-11 12:40:21 -0700

    tidying up math module

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,4 @@
 class Report < ActiveRecord::Base
-  #include ::CropModule::Maths
-  require './lib/crop_module/maths.rb'
+  include ::CropModule::Maths
+  #require './lib/crop_module/maths.rb'
 end
