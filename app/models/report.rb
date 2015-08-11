b9aa3511e5339cdc9b5380d1c347d99c6c789f38
Scott Loarie <loarie@gmail.com>
2015-08-11 13:24:36 -0700

    first step at including Twilio

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,3 @@
 class Report < ActiveRecord::Base
   include ::CropModule::Maths
-  #require './lib/crop_module/maths.rb'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'reports#index'
+  
+  post 'twilio/voice' => 'twilio#voice'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
