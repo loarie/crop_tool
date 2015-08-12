0690cc3680b8aa4add0ec32663f96559293481e4
Scott Loarie <loarie@gmail.com>
2015-08-11 23:28:23 -0700

    respond with estimate

--- app/models/text_message.rb
+++ app/models/text_message.rb
@@ -10,19 +10,29 @@ class TextMessage < ActiveRecord::Base
       error ||= true unless (code.count == 2 || (code.count == 3 && code[2].to_f != 0.0))
       
       if error
-        new_sentence = "Error #{body}"
+        new_sentence = "Error, we didn't understand #{body}"
       else
         if code.count == 2 #request
-          new_sentence = "The mean #{body}"
+          new_sentence = "Our #{code[1]} estimate for #{code[0]} is #{stats(code)}"
         else #report
           Report.create(value: code[2].to_f, crop: code[0].capitalize, statistic: code[1].capitalize)
-          new_sentence = "Thank you #{body}"
+          new_sentence = "Thank you for submitting your request of #{body}"
         end
       end
       TextMessage.create(to: from, from: phone_number, body: new_sentence)
     else
       send_outgoing
     end
+  end
+  
+  def stats(arr)
+    @reports = Report.where(crop: arr[0], statistic: arr[1])
+    @values = @reports.map(&:value)
+    @mean = ::CropModule::Maths.mean(@values)
+    @mean = 0 if @mean.nan?
+    @sd = ::CropModule::Maths.standard_deviation(@values)
+    @sd = 0 if @sd.nan?
+    return "#{(@mean * 1000).round} +/- #{(@sd * 1000).round} kg/ha"
   end
   
   def send_outgoing
