7043ffef66950f65f6a340b56b256821400bf056
Scott Loarie <loarie@gmail.com>
2015-08-13 12:28:15 -0700

    added ModelParameter model

--- app/models/text_message.rb
+++ app/models/text_message.rb
@@ -20,6 +20,7 @@ class TextMessage < ActiveRecord::Base
         else #report
           Report.create(value: code[2].to_f, crop: code[0].capitalize, statistic: code[1].capitalize)
           new_sentence = "Thank you for submitting your request of #{body}"
+          #update R model
         end
       end
       TextMessage.create(to: from, from: phone_number, body: new_sentence)
@@ -30,7 +31,9 @@ class TextMessage < ActiveRecord::Base
   
   def stats(arr)
     climate = get_climate(13.880746, -13.183594)
-    model_params = {beta: [34213.331619,-3195.578803,183.478251,64.716467,-1.463032], sigma2: 146643.8}
+    
+    #model_params = {beta: [34213.331619,-3195.578803,183.478251,64.716467,-1.463032], sigma2: 146643.8}
+    model_params = ModelParameter.where(country: "Senegal", crop: "Millet", statistic: "Yield").first
     beta = model_params[:beta]
     @sd = Math.sqrt(model_params[:sigma2])
     x_vals = [1.0, climate[:temp], climate[:prec], climate[:temp] ** 2, climate[:prec] ** 2]
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150812042725) do
+ActiveRecord::Schema.define(version: 20150813192336) do
+
+  create_table "model_parameters", force: :cascade do |t|
+    t.string   "country"
+    t.string   "crop"
+    t.string   "statistic"
+    t.string   "estimates"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "reports", force: :cascade do |t|
     t.float    "value"
