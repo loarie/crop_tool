c8efb3ce0cfddd3a93289a3c9611df9dd99e501b
Scott Loarie <loarie@gmail.com>
2015-08-15 09:54:08 -0700

    added more crop params

--- tools/load_model_params.rb
+++ tools/load_model_params.rb
@@ -1,7 +1,7 @@
 require "csv"
-file = File.join(Rails.root, 'tools', 'model_parameters.csv')
+file = File.join(Rails.root, 'tools', 'model_parameters_all.csv')
 File.open(file) do |f|
   CSV.foreach(f, col_sep:",", headers: true) do |csv_row|
-    ModelParameter.create(country: "Senegal", crop: csv_row[0], statistic: "Yield", estimated_params: "{\"beta\": [#{csv_row[1]},#{csv_row[2]},#{csv_row[3]},#{csv_row[4]},#{csv_row[5]}], \"sigma2\": #{csv_row[6]}}", priors: "{\"b0\": [#{csv_row[1]},#{csv_row[2]},#{csv_row[3]},#{csv_row[4]},#{csv_row[5]}], \"Vbcoef\": 1000, \"s1\": 2, \"s2\": #{csv_row[6]}}")
+    ModelParameter.create(country: "Senegal", crop: csv_row[0], statistic: "Yield", estimated_params: "{\"beta\": [#{csv_row[1]},#{csv_row[2]},#{csv_row[3]},#{csv_row[4]},#{csv_row[5]},#{csv_row[6]}], \"sigma2\": #{csv_row[7]}}", priors: "{\"b0\": [#{csv_row[1]},#{csv_row[2]},#{csv_row[3]},#{csv_row[4]},#{csv_row[5]},#{csv_row[6]}], \"Vbcoef\": 1000, \"s1\": 2, \"s2\": #{csv_row[7]}}")
   end
 end
