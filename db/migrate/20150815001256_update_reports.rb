49dd5a5583bb440eb262990cfa3bb2704546740e
Scott Loarie <loarie@gmail.com>
2015-08-14 17:27:01 -0700

    migration attempt take 2

--- db/migrate/20150815001256_update_reports.rb
+++ db/migrate/20150815001256_update_reports.rb
@@ -1,8 +1,8 @@
 class UpdateReports < ActiveRecord::Migration
   def change
-    change_column :reports, :prec, :float
-    change_column :reports, :temp, :float
-    change_column :reports, :lat, :float
-    change_column :reports, :lon, :float
+    change_column :reports, :prec, 'float USING CAST(prec AS float)'
+    change_column :reports, :temp, 'float USING CAST(temp AS float)'
+    change_column :reports, :lat, 'float USING CAST(lat AS float)'
+    change_column :reports, :lon, 'float USING CAST(lon AS float)'
   end
 end
