3ac76dc175bc396626570ebfc8d19d14326ab8e2
Scott Loarie <loarie@gmail.com>
2015-08-16 11:51:31 -0700

    string to float issue

--- tools/load_model_params.rb
+++ tools/load_model_params.rb
@@ -5,7 +5,7 @@ File.open(file) do |f|
     vbcoef = 500000
     sigma2 = csv_row[7]
     s1 = 10 + 1
-    s2 = 10 * sigma2
+    s2 = 10 * sigma2.to_f
     ModelParameter.create(country: nil, crop: csv_row[0], statistic: "Yield", estimated_params: "{\"beta\": [#{csv_row[1]},#{csv_row[2]},#{csv_row[3]},#{csv_row[4]},#{csv_row[5]},#{csv_row[6]}], \"sigma2\": #{csv_row[7]}}", priors: "{\"b0\": [#{csv_row[1]},#{csv_row[2]},#{csv_row[3]},#{csv_row[4]},#{csv_row[5]},#{csv_row[6]}], \"Vbcoef\": #{vbcoef}, \"s1\": #{s1}, \"s2\": #{s2}}")
   end
 end
