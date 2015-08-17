75b15e4cbecb8588cea90b89d37cf37b4166c904
Scott Loarie <loarie@gmail.com>
2015-08-16 23:29:19 -0700

    home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  
+  get 'static_pages/home'
+  get 'static_pages/about'
+
   resources :reports
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'reports#index'
+  root 'static_pages#home'
   
   get 'model_parameters_estimate', to: 'model_parameters#estimate'
   post 'twilio/voice' => 'twilio#voice'
